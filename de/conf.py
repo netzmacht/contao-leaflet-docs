c1fbde4a708a2ec93c6a65a8d9a6732c7fe93950
David Molineus <david.molineus@netzmacht.de>
2019-02-21 10:08:54 +0100

    Use copyright range.

--- de/conf.py
+++ de/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Leaflet Maps for Contao'
-copyright = u'2019, netzmacht David Molineus'
+copyright = u'2016-2019, netzmacht David Molineus'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
